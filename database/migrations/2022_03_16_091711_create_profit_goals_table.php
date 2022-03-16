60c06acbde34e82e8086635362a1dc80a83f669a
itzRoy <royissa3@gmail.com>
2022-03-16 21:22:58 +0200

    1

--- database/migrations/2022_03_16_091711_create_profit_goals_table.php
+++ database/migrations/2022_03_16_091711_create_profit_goals_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Currency_controller;
 
 return new class extends Migration
 {
@@ -13,14 +14,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('profit_goals', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->date('year');
+            $table->integer('amount');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('profit_goals');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Profit_goal_controller;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/profit-goals', [Profit_goal_controller::class, 'showall']);
+Route::get('/profit-goal/{id}', [Profit_goal_controller::class, 'show']);
+
+Route::post('/profit-goal-create', [Profit_goal_controller::class, 'create']);
+
+Route::put('/profit-goal-update/{id}', [Profit_goal_controller::class, 'update']);
+
