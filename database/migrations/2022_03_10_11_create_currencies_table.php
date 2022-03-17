1dcbd883c16c4bb2521e50f2983e947d381eca76
Bakrifly <bakri.hmouda@gmail.com>
2022-03-17 15:10:09 +0200

    staged missing files

---
+++ database/migrations/2022_03_10_11_create_currencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name');
+            $table->float('exchange_rate', 8, 4);
+            $table->string('currency_code', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
---
+++ database/migrations/2022_03_10_33_create_recurrences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recurrences', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->dateTimeTz('start_date');
+            $table->dateTimeTz('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recurrences');
+    }
+};
